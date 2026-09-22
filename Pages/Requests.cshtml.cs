ed85fec967202bff0daad4fe1e0311157b1e8685
Moriswi Simon <simonmothoka40@gmail.com>
2026-09-22 16:27:43 +0200

    Add service requests page

---
+++ Pages/Requests.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceDesk.Pages
+{
+    public class RequestsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
