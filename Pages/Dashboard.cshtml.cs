cd132c04235deb5e8b8c1d42e435751ec9d2b0f9
Moriswi Simon <simonmothoka40@gmail.com>
2026-09-22 15:42:09 +0200

    Initial ServiceDesk project

---
+++ Pages/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceDesk.Pages
+{
+    public class DashboardModel : PageModel
+    {
+        public void onGet()
+        {
+            
+        }
+    }
+}
