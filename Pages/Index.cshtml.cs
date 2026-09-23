3e98c8230de48c93d662c6b47aab3e520ab6b184
Moriswi Simon <simonmothoka40@gmail.com>
2026-09-23 15:45:19 +0200

    Add live dashboard request counts

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,12 +1,27 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServiceDesk.Data;
 
 namespace ServiceDesk.Pages;
 
 public class IndexModel : PageModel
 {
+    private readonly ServiceDeskContext _context;
+
+    public IndexModel(ServiceDeskContext context)
+    {
+        _context = context;
+    }
+
+    public int OpenRequests { get; set; }
+    public int InProgressRequests { get; set; }
+    public int ResolvedRequests { get; set; }
+
     public void OnGet()
     {
+        OpenRequests = _context.ServiceRequests.Count(r => r.Status == "Open");
 
+        InProgressRequests = _context.ServiceRequests.Count(r => r.Status == "In Progress");
+
+        ResolvedRequests = _context.ServiceRequests.Count(r => r.Status == "Resolved");
     }
 }
