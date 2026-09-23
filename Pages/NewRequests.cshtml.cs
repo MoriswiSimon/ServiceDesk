39d9026dfe9f1143c5f990c22eab877b9d1a0afd
Moriswi Simon <simonmothoka40@gmail.com>
2026-09-23 15:15:50 +0200

    Save service requests to database

--- Pages/NewRequests.cshtml.cs
+++ Pages/NewRequests.cshtml.cs
@@ -1,10 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServiceDesk.Data;
+using ServiceDesk.Models;
 
 namespace ServiceDesk.Pages
 {
     public class NewRequestModel : PageModel
     {
+        private readonly ServiceDeskContext _context;
+
+        public NewRequestModel(ServiceDeskContext context)
+        {
+            _context = context;
+        }
+
         [BindProperty]
         public string RequestTitle { get; set; } = "";
 
@@ -22,6 +31,20 @@ namespace ServiceDesk.Pages
 
         public IActionResult OnPost()
         {
+            var request = new ServiceRequest
+            {
+                RequestTitle = RequestTitle,
+                Category = Category,
+                Priority = Priority,
+                SubmittedBy = SubmittedBy,
+                Description = Description,
+                Status = "Open",
+                DateSubmitted = DateTime.Now
+            };
+
+            _context.ServiceRequests.Add(request);
+            _context.SaveChanges();
+
             return RedirectToPage("/Requests");
         }
     }
--- Pages/Requests.cshtml.cs
+++ Pages/Requests.cshtml.cs
@@ -1,11 +1,26 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ServiceDesk.Data;
+using ServiceDesk.Models;
 
 namespace ServiceDesk.Pages
 {
     public class RequestsModel : PageModel
     {
-        public void OnGet()
+        private readonly ServiceDeskContext _context;
+
+        public RequestsModel(ServiceDeskContext context)
         {
+            _context = context;
+        }
+
+        public List<ServiceRequest> Requests { get; set; } = new();
+
+        public async Task OnGetAsync()
+        {
+            Requests = await _context.ServiceRequests
+                .OrderByDescending(r => r.DateSubmitted)
+                .ToListAsync();
         }
     }
 }
