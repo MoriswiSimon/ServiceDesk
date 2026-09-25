73b21ff95468b1ab533521d5446830121910db52
Moriswi Simon <simonmothoka40@gmail.com>
2026-09-25 14:38:05 +0200

    Connect dashboard to request database

--- Pages/Dashboard.cshtml.cs
+++ Pages/Dashboard.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServiceDesk.Data;
 
 namespace ServiceDesk.Pages
 {
     public class DashboardModel : PageModel
     {
-        public void onGet()
+        private readonly ServiceDeskContext _context;
+
+        public DashboardModel(ServiceDeskContext context)
         {
-            
+            _context = context;
+        }
+
+        public int OpenRequests { get; set; }
+        public int InProgressRequests { get; set; }
+        public int ResolvedRequests { get; set; }
+        public int TotalRequests { get; set; }
+
+        public void OnGet()
+        {
+            OpenRequests = _context.ServiceRequests.Count(r => r.Status == "Open");
+
+            InProgressRequests = _context.ServiceRequests.Count(r => r.Status == "In Progress");
+
+            ResolvedRequests = _context.ServiceRequests.Count(r => r.Status == "Resolved");
+
+            TotalRequests = _context.ServiceRequests.Count();
         }
     }
 }
